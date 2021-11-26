82bcbafbd9cea732f7212eed91914378bec45823
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2021-11-26 23:36:13 +0100

    valdition

--- src/components/gallery/Overlay.tsx
+++ src/components/gallery/Overlay.tsx
@@ -12,35 +12,101 @@ import { actions } from '../../features/hamstersReducer'
 interface OverlayProps {
     close: () => void;
     addHamster: (hamster: Hamster) => void;
-    setHamsterData:(array:Hamster[])=>void;
+    setHamsterData: (array: Hamster[]) => void;
 }
 
-const Overlay = ({ close, addHamster,setHamsterData }: OverlayProps) => {
+const Overlay = ({ close, setHamsterData }: OverlayProps) => {
     const [name, setName] = useState<string>('')
-    const [age, setAge] = useState<number>(0)
+    const [age, setAge] = useState<number>(1)
     const [favFood, setFavFood] = useState<string>('')
     const [loves, setLoves] = useState<string>('')
     const [imgName, setImgName] = useState<string>('')
 
+    //validition
+    const [isChecked, setChecked] = useState<boolean>(false)
+    const [clickedNameField, setClickedNameField] = useState<boolean>(false)
+    const [clickedLoveField, setClickedLoveField] = useState<boolean>(false)
+    const [clickedFoodField, setClickedFoodField] = useState<boolean>(false)
 
+    const nameIsValid = validateName(name)
+    const loveIsValid = validateLove(loves)
+    const foodIsValid = validateFood(favFood)
+    const imgIsValid = validateImg(imgName, isChecked)
+
+
+
+
+    // valdition
+    function validateName(name: string): boolean {
+        return name.length >= 2
+    }
+
+    function validateLove(loves: string): boolean {
+        return loves.length >= 2
+    }
+
+    function validateFood(favFood: string): boolean {
+        return favFood.length >= 2
+    }
+
+    function validateImg(imgName: string, isChecked: boolean): boolean {
+
+        let testString = imgName.split('')
+        let testArray1 = testString.slice(0, 8)
+        let testArray2 = testString.slice(0, 7)
+        let startHost = testArray1.join('') === 'https://' || testArray2.join('') === 'http://'
+        let pathCheck = testString.slice(8).includes('.') && testString.slice(8).includes('/')
+        let hamsterImg1 = testString.slice(0, 8)
+        let hamsterImg2 = testString.slice(-4)
+
+        let urlOk = startHost && pathCheck
+
+        let fileNameOk = hamsterImg2.join('') === '.jpg' && hamsterImg1.join('') === 'hamster-'
+        return urlOk || fileNameOk || isChecked
+    }
+
+    const handleImgName = (input: string) => {
+        setImgName(input)
+    }
+    const handleRandomImg = (checked:boolean) => {
+		setChecked(checked)
+		if ( checked ) {
+			setImgName(`/hamster-${Math.ceil(Math.random()*40)}.jpg`)
+		}
+	}
+
+    const handleName = (input: string) => {
+        setName(input)
+        setClickedNameField(true)
+    }
+
+    const handleLove = (input: string) => {
+        setLoves(input)
+        setClickedLoveField(true)
+    }
+
+    const handleFavFood = (input: string) => {
+        setFavFood(input)
+        setClickedFoodField(true)
+    }
 
 
     async function sendRequest() {
-		try {
-			const response = await fetch('/hamsters')
-			const data = await response.json()
-			setHamsterData(data)
-		} catch (error) {
-			console.log('error:', error);
-		}
-	}
-   
+        try {
+            const response = await fetch('/hamsters')
+            const data = await response.json()
+            setHamsterData(data)
+        } catch (error) {
+            console.log('error:', error);
+        }
+    }
+
 
     const dispatch = useDispatch()
 
     const postHamster = async () => {
         // förbered Hamster-objekt och anropa addHamster-funktionen
-        const hamster= {
+        const hamster = {
             // Hämta riktiga värden från formuläret
             age: age, defeats: 0, favFood: favFood, games: 0, imgName: imgName, loves: loves, name: name, wins: 0
         }
@@ -53,21 +119,19 @@ const Overlay = ({ close, addHamster,setHamsterData }: OverlayProps) => {
                 },
                 body: JSON.stringify(hamster)
             }
-           
+
         )
-      
+
         console.log(JSON.stringify(hamster));
         const newHamster = await response.json()
         console.log(newHamster)
-        console.log("Success", newHamster);
         await dispatch(actions.addHamster(newHamster))
         sendRequest()
 
         close()
 
-        
-     }
-      
+
+    }
     return (
         <div className="overlay">
             <div className="dialog">
@@ -76,25 +140,41 @@ const Overlay = ({ close, addHamster,setHamsterData }: OverlayProps) => {
 
                     <input type="text" placeholder="Hamsters name"
                         value={name}
-                        onChange={e => setName(e.target.value)} />
+                        onChange={e => handleName(e.target.value)} className={nameIsValid ? 'valid' : ' not-valid'} />
+                    {!nameIsValid && clickedNameField ?
+                        <span >Name is empty or not correct.</span>
+                        : null
+                    }
                     <input type="text" placeholder="Favorite food"
                         value={favFood}
-                        onChange={e => setFavFood(e.target.value)} />
-
+                        onChange={e => handleFavFood(e.target.value)} className={foodIsValid ? 'valid' : ' not-valid'} />
+                    {!foodIsValid && clickedFoodField ?
+                        <span >At least two letters.</span>
+                        : null
+                    }
                     <input type="text" placeholder="Loves"
                         value={loves}
-                        onChange={e => setLoves(e.target.value)} />
+                        onChange={e => handleLove(e.target.value)} className={loveIsValid ? 'valid' : ' not-valid'} />
+                    {!loveIsValid && clickedLoveField ?
+                        <span  >Hoppy is empty.</span>
+                        : null
+                    }
 
                     <input type="number" min="1" value={age}
                         onChange={e => setAge(e.target.valueAsNumber)} />
 
-                    <input type="url" placeholder="Image URL" value={imgName}
-                        onChange={e => setImgName(e.target.value)} />
 
+                    <input type="url" placeholder="Image URL" value={imgName} disabled={isChecked}
+                        onChange={e => handleImgName(e.target.value)} className={imgIsValid ? 'valid' : ' not-valid'} />
+                    {imgIsValid && !isChecked ?
+                        <img src={imgName.includes('http') ? imgName : `/img/${imgName}`} alt="hamsterbild" />
+                        : null
+                    }
+                    <input type="checkbox" onChange={() => handleRandomImg(!isChecked)} /><label>tryck here for random bild</label>
                 </form>
                 <div>
 
-                    <button className="addhamster" onClick={()=> { postHamster();}}> Add hamster </button>
+                    <button className="addhamster" onClick={() => { postHamster(); sendRequest()}} disabled={!nameIsValid || !loveIsValid || !foodIsValid || !imgIsValid}> Add hamster </button>
                     <button className="close-btn" onClick={close}> x </button>
                 </div>
 
